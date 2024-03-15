a58e2f97e112c223604aa724a424f867a6805383
Lucas <lucasgomesm1808@gmail.com>
2024-03-15 07:29:38 -0300

    movendo arquivos

--- ExemploEQL.rb
+++
@@ -1,8 +0,0 @@
-linguagem1 = "java"
-linguagem2 = "ruby"
-
-
-
-if linguagem1.eql?(linguagem1)
-    puts("Escolha errada, é: #{linguagem2}")
-end
--- ExemploHash.rb
+++
@@ -1,2 +0,0 @@
-range = (1..5)
-puts range.to_a.inspect

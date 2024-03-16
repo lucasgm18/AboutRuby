8966fc389fc29ff85ff337859fc95a7eb0844714
Lucas <lucasgomesm1808@gmail.com>
2024-03-16 08:20:07 -0300

    linha de comentário

--- Edu/Lucas/ExerciciosLista1/Exe13.rb
+++ Edu/Lucas/ExerciciosLista1/Exe13.rb
@@ -9,3 +9,5 @@ cotacao_real = 4.99
 conversao_real = dolar * cotacao_real
 
 puts "A conversão para reais foi: #{conversao_real.round(2)}"
+
+# Subi para o repositório com o valor de 4,99 pois eu estava usando o câmbio atual 16/03/2024, apenas para tirar uma dúvida.
